8041cd1f2022d346e9ab87a00a16c37b419fe11f
Phan Anh <phananh12042003@gmail.com>
2021-08-20 17:25:22 -0400

    add Question1.py

---
+++ Homework/Homework10/Homework10.js
@@ -0,0 +1,3 @@
+//1 Tạo 1 mảng. 
+//2 Viết function in ra "Yes" nếu tất cả phần tử của mảng chẵn, nếu không thì in ra "No".
+//3 Sử dụng Array.forEach() để in ra tất cả các phần tử chia hết cho 5 của mảng.
---
+++ Homework/Homwork2&&3/Question1/Question1.py
@@ -0,0 +1,3 @@
+import math
+for i in range(1,math.ceil(math.sqrt(1000))):
+    print(i**2)
